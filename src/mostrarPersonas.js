ac9b0ad36cfdd7a2d52792fa6ebb249dea8908f8
sebastianalamina <alamina@ciencias.unam.mx>
2020-11-09 14:31:55 -0600

    Correcciones sobre script ejercicio 4 (mostrarPersonas.js).

--- src/mostrarPersonas.js
+++ src/mostrarPersonas.js
@@ -1,15 +1,15 @@
 // La biblioteca request-promise es obsoleta, por lo
 // que, en su lugar, se utilizará "https".
-const https = require('https')
-const url = 'https://reclutamiento-14cf7.firebaseio.com/personas.json'
+const https = require('https');
+const url = 'https://reclutamiento-14cf7.firebaseio.com/personas.json';
 
 // Se utiliza el método 'get' pues es la versión simplificada
 // del método 'request' cuando se trata del verbo GET...
 var x = https.get(url, (res) => {
   // Imprimimos información sobre la consulta...
   console.log(`Código de estado: ${res.statusCode} - ${res.statusMessage}`);
-  console.log(`Headers:`)
-  console.log(res.headers)
+  console.log(`Headers:`);
+  console.log(res.headers);
   // Leemos los datos, pedazo por pedazo. El evento 'data'
   // se emite cuando el flujo le envía un pedazo de datos
   // al consumidor.
@@ -21,7 +21,7 @@ var x = https.get(url, (res) => {
     // Convertimos la cadena de datos crudos (JSON) en un
     // objeto, para imprimirse de forma más amigable.
     const jsonObject = JSON.parse(raw);
-    console.log(`Cuerpo:`)
+    console.log(`Cuerpo:`);
     console.log(jsonObject);
   });
 });
